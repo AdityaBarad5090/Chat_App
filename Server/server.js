4637a7a6244f26d5f4efd7fc3ff410443efdd4bd
AdityaBarad5090 <adityasinhbarad@gmail.com>
2026-08-31 12:09:16 +0530

    Changes For Cors

--- Server/server.js
+++ Server/server.js
@@ -15,7 +15,11 @@ const chatSocket = require("./socket/chatSocket");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 app.use(express.json());
 
